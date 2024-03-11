e03f84cbea89144cff1e120819b3459cfeef6333
fcu-d0813191 <wp900622@gmail.com>
2024-03-11 23:10:13 +0800

    second commit

--- index/models.py
+++ index/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=10)
+    def __str__(self):
+        return str(self.name)
+
+class Area(models.Model):
+    name = models.CharField(max_length=10)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.name)
+
+class Product(models.Model):
+    name = models.CharField(max_length=10)
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.name)
+
+
--- index/views.py
+++ index/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from commodity.models import *
+
+
 # Create your views here.
+def indexView(request):
+    title = "首頁"
+    classContent= ''
+    commodityInfo = CommodityInfo.objects.order_by('-sold').all()[:8]
+
+    types = Type.objects.all()
